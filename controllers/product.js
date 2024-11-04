402a8a2d293ed1e4ad446911626abe40e027a3b9
iftikarjahan <iftikarjahan22@gmail.com>
2024-11-04 13:01:20 +0530

    created a query object and chained few methids to it

--- controllers/product.js
+++ controllers/product.js
@@ -1,10 +1,54 @@
+const Product=require("../model/products");
+const { options } = require("../routes/products");
+
+
 const getAllProductsStatic=async(req,res,next)=>{
-    throw new Error("Testing the new express-async-error🍃🍃");
-    res.status(200).json({msg:"Static products testing route"});
+    const {name}=req.query;
+    /*
+    ->Instead of passing the req.query object directly, we should create the query object
+    and then pass it to the find method
+    */ 
+    const queryObject={};
+    // if(name){
+    //     queryObject.name={$regex:name,$options:"i"}
+    // }
+
+    const products=await Product.find(queryObject).sort("-price -name");
+    res.status(200).json({products,nbHits:products.length});
 }
 
 const getAllProducts=async(req,res,next)=>{
-    res.status(200).json({msg:"producst route"});
+    const {featured,company,name,sort}=req.query;
+    let queryObject={};
+    if(featured){
+        queryObject.featured=featured
+    }
+    if(company){
+        queryObject.company=company;
+    }
+    if(name){
+        queryObject.name={$regex:name,$options:"i"};
+    }
+    let result=Product.find(queryObject);   //this creates a query object
+    if(sort){
+        sortList=sort.split(",").join(" ");
+        // console.log(sortList);
+        result=result.sort(sortList);
+    }
+    else{
+        result=result.sort("createdAt");
+    }
+    /*
+    ->Note that the result is a query object. 
+    ->When we use a method like Product.find({}), mongoose creates a query object
+    ->However the query operation is not yet executed until it is explicitly called with'
+    the await syntax
+    ->This gives you flexibility to chain multiple methods together
+    */ 
+    console.log(typeof(result));   //object
+    
+    const products=await result;
+    res.status(200).json({products:products,nbHits:products.length});
 }
 
 /*
